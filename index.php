643f87640b2f4b7abe3b9993db86e243c6e0766c
Marcio Antunes <cgslivre@cgslivre.com>
2014-11-06 10:33:36 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+
 <?php
+
+$postData = $_POST;
+if(isset($_GET['doAction']))
+{
+	$action = $_GET['doAction'];
+	switch($action)
+	{
+		case 'insert':
+			
+		break;
+		case 'update':
+			
+		break;
+		case 'delete':
+			
+		break;
+	}
+}
+?>
+
+<html>
+	<head></head>
+	<body>
 	
+	
+	</body>
+</html>
+
+<?php
+	endif;	
 ?>
