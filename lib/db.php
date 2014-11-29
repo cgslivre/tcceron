9c79afdb017d09e080075b6b3ad3d51e258e18f0
Marcio Antunes <cgslivre@cgslivre.com>
2014-11-29 08:39:23 -0200

    Update db.php

--- lib/db.php
+++ lib/db.php
@@ -1,3 +1,13 @@
 <?php
-$pdo = ;
+
+// Variáveis de acesso ao bando de dados
+$host   = 'localhost';
+$dbname = 'nome_do_banco_de_dados';
+$user   = 'usuario_do_banco';
+$pass   = 'senha_do_banco';
+
+// Cria uma instancia da Super Classe PDO
+// http://php.net/manual/en/book.pdo.php
+$pdo = new PDO("mysql:host=$host;dbname=$dbname;", $user, $pass);
+
 ?>
